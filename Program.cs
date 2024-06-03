dcf84f61f4dd0a6b368a4fb6fb7ea2aee3b20495
Radosław Domagała <contact.dompollab@gmail.com>
2024-06-03 10:48:16 +0200

    Added files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Give an integer: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Give a power of the integer: ");
+            int y = int.Parse(Console.ReadLine());
+            double powX = Math.Pow(x, y);
+
+            Console.WriteLine($"Result: " + powX);
+        }
+    }
+}
